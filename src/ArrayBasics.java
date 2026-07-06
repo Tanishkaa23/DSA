21b7e4e44264c262984c272bd112a285dfd3b317
Tanishkaa23 <tanishkagupta203@gmail.com>
2026-07-06 16:38:23 +0530

    Started Array Basics

---
+++ src/ArrayBasics.java
@@ -0,0 +1,13 @@
+public class ArrayBasics {
+    public static void main(String[] args){
+
+        //Traverse elements of an array
+
+        int arr[] = {1,2,3,4,5};
+
+        for(int i=0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+}
