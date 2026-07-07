2f986ddb4e2b7df85a0863871dc61b95d800cd64
Tanishkaa23 <tanishkagupta203@gmail.com>
2026-07-07 12:48:18 +0530

    remove duplicates, while keeping atmost 2 elements and return first k elements

---
+++ src/Arrays/RemoveDuplicate2.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class RemoveDuplicate2 {
+    public static void main(String[] args) {
+
+        int arr[]={1,1,1,2,2,3,3,3};
+
+        int k=2;
+
+        for(int i=2; i<arr.length; i++){
+            if(arr[i]!=arr[k-2]){
+                arr[k]=arr[i];
+                k++;
+            }
+        }
+
+        for(int i=0; i<k; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+        System.out.println(k);
+    }
+}
