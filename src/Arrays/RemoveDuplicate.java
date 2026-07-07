1e8a9f62d4d902e14f1ad7452341e4baf2497e3e
Tanishkaa23 <tanishkagupta203@gmail.com>
2026-07-07 13:21:45 +0530

    remove duplicates, code fixes

--- src/Arrays/RemoveDuplicate.java
+++ src/Arrays/RemoveDuplicate.java
@@ -7,20 +7,20 @@ public class RemoveDuplicate {
 
         //remove duplicate in sorted array and return first k elem
 
-        int arr[] = {1,2,2,3,3}; // => {1,2,3} =3
+        int arr[] = {1,2,2,3,3,3,3,3,4,4,4,5,5,5,6};
 
-        System.out.println(Arrays.toString(arr));
 
-        int k=0;
+        int k=1;
 
-        for(int i = 0; i < arr.length-1; i++){
-             if(arr[i]!=arr[i+1]){
+        for(int i = 1; i < arr.length; i++){
+             if(arr[i]!=arr[k-1]){
                  arr[k]=arr[i];
                  k++;
              }
         }
-        arr[k]=arr[arr.length-1];
-        k++;
+//        arr[k]=arr[arr.length-1];
+//        k++;
+        //special case/handling when used [i+1]
 
         for (int i = 0; i < k; i++) {
             System.out.print(arr[i] + " ");
