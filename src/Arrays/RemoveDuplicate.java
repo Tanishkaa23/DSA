a6854f64e3eff1c20ddb3ad7f6db69122588fe94
Tanishkaa23 <tanishkagupta203@gmail.com>
2026-07-07 12:47:40 +0530

    remove duplicates and return first k elements

--- src/Arrays/RemoveDuplicate.java
+++ src/Arrays/RemoveDuplicate.java
@@ -1,21 +1,30 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class RemoveDuplicate {
     public static void main(String[] args) {
 
-        //remove duplicate in sorted array
+        //remove duplicate in sorted array and return first k elem
 
-        int arr[] = {1,1,2,3,3}; // => {1,2,3} =3
+        int arr[] = {1,2,2,3,3}; // => {1,2,3} =3
 
-        int count=1;
+        System.out.println(Arrays.toString(arr));
 
-        for(int i=0; i< arr.length-1; i++) {
-            if(arr[i]!=arr[i+1]){
-                arr[count]=arr[i];
-                count++;
-            }
+        int k=0;
+
+        for(int i = 0; i < arr.length-1; i++){
+             if(arr[i]!=arr[i+1]){
+                 arr[k]=arr[i];
+                 k++;
+             }
         }
-        System.out.println(count);
+        arr[k]=arr[arr.length-1];
+        k++;
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
